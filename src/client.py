48fb2f72e78c8d5fd37afa213da28ead80b01769
jwmsg0525 <jaewan@jwmsg.me>
2020-10-27 12:21:56 +0900

    gbn dev start

--- src/client.py
+++ src/client.py
@@ -1,12 +1,16 @@
-from .modules.RDTProtocol.RdtClient import RdtClient
+from .modules.RdtController.RdtClient import RdtClient
+from .modules.GbnController.GbnClient import GbnClient
+from .modules.SrController.SrClient import SrClient
 
 class Client:
-    def __init__(self, config, message_pool):
-        
-    def run_sr_class():
+    def __init__(self, messages):
+        self.messages = messages
+
+    def run_sr_class(self):
+        self.data_class = SrClient()
     
+    def run_gbn_class(self):
+        self.data_class = SrClient()
     
-    def run_gbn_class():
-    
-    
-    def run_rdt_class
+    def run_rdt_class(self):
+        self.data_class = SrClient()
--- src/server.py
+++ src/server.py
@@ -1,5 +1,16 @@
-from .modules.RDTProtocol.RdtServer import RdtServer
+from .modules.RdtController.RdtServer import RdtServer
+from .modules.GbnController.GbnServer import GbnServer
+from .modules.SrController.SrServer import SrServer
 
 class Server:
-    def __init__(self, config):
-        
+    def __init__(self, messages):
+        self.messages = messages
+
+    def run_sr_server(self):
+        self.data_class = SrClient()
+    
+    def run_gbn_server(self):
+        self.data_class = SrClient()
+    
+    def run_rdt_server(self):
+        self.data_class = SrClient()
